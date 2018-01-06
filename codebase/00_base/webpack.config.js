34733e2383c03e5467f2477ddce967be33ce820c
Karol Kowalczuk <karolkowalczuk1992@gmail.com>
2018-01-06 15:11:01 +0100

    Finish basic webpack configuration (dev-server, html-webpack, babel)

--- codebase/00_base/webpack.config.js
+++ codebase/00_base/webpack.config.js
@@ -1,9 +1,41 @@
 ﻿const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+const SRC_PATH = path.resolve(__dirname, "./src")
+const DIST_PATH = path.resolve(__dirname, "./dist")
 
 module.exports = {
-  entry: "./src/index.jsx",
+  entry: path.join(SRC_PATH, "index.jsx"),
+  /* Destination of "packed" JS bundle */
   output: {
-    path: path.resolve(__dirname, "./dist"),
-	filename: "bundle.js",
-  }
+    path: DIST_PATH,
+    filename: "app.bundle.js",
+  },
+  /* Configure webpack-dev-server */
+  devServer: {
+    port: 5555,
+    open: true,
+  },
+  plugins: [
+    /* Injects JS bundle into desired HTML file */
+    new HtmlWebpackPlugin({
+      template: path.join(SRC_PATH, "index.html"),
+    }),
+  ],
+  /* Apply babel-loader on all files with .js and .jsx extensions */
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          "babel-loader",
+        ],
+      }
+    ],
+  },
+  /* Enable importing files without specifying the extensions */
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
 }
