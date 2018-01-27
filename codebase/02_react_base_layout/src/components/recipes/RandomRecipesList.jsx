a398b74b7662e01a5e1343b383ce0447855ac131
Michał Dettlaff <michal.dettlaff@best.com.pl>
2018-01-27 12:17:10 +0100

    prop types

--- codebase/02_react_base_layout/src/components/recipes/RandomRecipesList.jsx
+++ codebase/02_react_base_layout/src/components/recipes/RandomRecipesList.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import PropTypes from "prop-types"
 
 import RecipeCard from "./RecipeCard"
 
@@ -7,12 +8,17 @@ const RandomRecipesList = (props) => {
     <fieldset className="row z-depth-1 yellow lighten-4">
       <h4>Random recipes</h4>
       <div className="row">
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
+        <RecipeCard title="Burg 1" description="something something" imageUrl="http://www.seriouseats.com/recipes/images/2017/06/20170617-bulgogi-burger-matt-clifton-1.jpg" />
+        <RecipeCard title="Pizza" description="something something 2" imageUrl="http://www.seriouseats.com/recipes/images/2017/06/20170617-bulgogi-burger-matt-clifton-1.jpg" />
+        <RecipeCard title="Schaboschchaque" description="5432" imageUrl="http://www.seriouseats.com/recipes/images/2017/06/20170617-bulgogi-burger-matt-clifton-1.jpg" />
       </div>
     </fieldset>
   )
 }
 
+RecipeCard.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
+}
 export default RandomRecipesList
--- codebase/02_react_base_layout/src/components/recipes/RecipeCard.jsx
+++ codebase/02_react_base_layout/src/components/recipes/RecipeCard.jsx
@@ -1,19 +1,28 @@
 import React from "react"
 
-const RecipeCard = (props) => {
-  return (
-    <div className="col m12 l4">
-      <div className="card medium yellow lighten-5">
-        <div className="card-image">
-          <img src="http://www.seriouseats.com/recipes/images/2017/06/20170617-bulgogi-burger-matt-clifton-1.jpg" />
-          <span className="card-title">King Burger</span>
-        </div>
-        <div className="card-content">
-          <p>Here are the instructions to make the perfect burger! Although it may not be the healthiest, use medium ground beef for moistness & flavour.</p>
+class RecipeCard extends React.Component {
+  render() {
+    return (
+      <div className="col m12 l4">
+        <div className="card medium yellow lighten-5">
+          <div className="card-image">
+            <img src={this.props.imageUrl} />
+            <span className="card-title">{this.props.title}</span>
+          </div>
+          <div className="card-content">
+            <p>{this.props.description}</p>
+          </div>
         </div>
       </div>
-    </div>
+    )
+  }
+}
+
+/*
+const RecipeCard = (props) => {
+  return (
   )
 }
+*/
 
 export default RecipeCard
