5cc25e95e46f398a3deedf0d86639f541826da8f
Michał Dettlaff <michal.dettlaff@best.com.pl>
2018-01-28 09:40:47 +0100

    color picker - listener

--- codebase/06_pure_components/src/components/colorpicker/ColorPicker.jsx
+++ codebase/06_pure_components/src/components/colorpicker/ColorPicker.jsx
@@ -2,9 +2,15 @@ import React from "react"
 import PropTypes from "prop-types"
 
 export default class ColorPicker extends React.PureComponent {
+
+  onInputChange(event) {
+    console.log(event.target.value)
+  }
+
   render() {
+    const onInputChange = this.onInputChange
     return (
-      <input type="text" />
+      <input type="text" onChange={onInputChange} />
     )
   }
 }
