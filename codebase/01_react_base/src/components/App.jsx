0aba83f087d8b04766ffa386124f07a6d91d02ac
Michał Dettlaff <michal.dettlaff@best.com.pl>
2018-01-28 11:22:40 +0100

    chat - input

--- codebase/01_react_base/src/components/App.jsx
+++ codebase/01_react_base/src/components/App.jsx
@@ -1,15 +1,29 @@
 import React from "react"
 
 export default class App extends React.PureComponent {
+
   constructor(props) {
     super(props)
 
+    this.sendMessage = this.sendMessage.bind(this)
+    this.onInputChange = this.onInputChange.bind(this)
+
     this.state = {
       messages: [{ "name": "misza", "message": "wot wot wot" }, { "name": "wolodya", "message": "trabaho" }],
+      messageToSend: "foo"
     }
   }
 
+  sendMessage() {
+    console.log('sending message: ' + this.state.messageToSend)
+  }
+
+  onInputChange(event) {
+    this.setState({ messageToSend: event.target.value })
+  }
+
   render() {
+    const sendMessage = this.sendMessage
     return (
       <div>
         <h1>Dżit Czat</h1>
@@ -21,8 +35,8 @@ export default class App extends React.PureComponent {
           }
         </ul>
         Message content:
-        <input type="text"></input>
-        <button>Send</button>
+        <input type="text" value={this.state.messageToSend} onChange={this.onInputChange} />
+        <button onClick={sendMessage}>Send</button>
       </div>
     )
   }
